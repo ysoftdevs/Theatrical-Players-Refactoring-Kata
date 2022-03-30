615dc285850284c8ce6f426f86592e953fb3daa7
Göth Radim <radim.goeth@ysoft.com>
2022-03-30 13:30:29 +0200

    upgrade to net6, update dependencies, conventions

--- csharp/TheatricalPlayersRefactoringKata.Tests/StatementPrinterTests.cs
+++ csharp/TheatricalPlayersRefactoringKata.Tests/StatementPrinterTests.cs
@@ -11,18 +11,18 @@ namespace TheatricalPlayersRefactoringKata.Tests
     {
         [Test]
         [UseReporter(typeof(DiffReporter))]
-        public void test_statement_plain_text_example()
+        public void Test_statement_plain_text_example()
         {
             var plays = new Dictionary<string, Play>();
             plays.Add("hamlet", new Play("Hamlet", "tragedy"));
             plays.Add("as-like", new Play("As You Like It", "comedy"));
             plays.Add("othello", new Play("Othello", "tragedy"));
 
-            Invoice invoice = new Invoice("BigCo", new List<Performance>{new Performance("hamlet", 55),
+            var invoice = new Invoice("BigCo", new List<Performance>{new Performance("hamlet", 55),
                 new Performance("as-like", 35),
                 new Performance("othello", 40)});
             
-            StatementPrinter statementPrinter = new StatementPrinter();
+            var statementPrinter = new StatementPrinter();
             var result = statementPrinter.Print(invoice, plays);
             
             Approvals.Verify(result);
@@ -30,18 +30,18 @@ namespace TheatricalPlayersRefactoringKata.Tests
 
         [Test]
         [UseReporter(typeof(DiffReporter))]
-        public void test_statement_html_example()
+        public void Test_statement_html_example()
         {
             var plays = new Dictionary<string, Play>();
             plays.Add("hamlet", new Play("Hamlet", "tragedy"));
             plays.Add("as-like", new Play("As You Like It", "comedy"));
             plays.Add("othello", new Play("Othello", "tragedy"));
 
-            Invoice invoice = new Invoice("BigCo", new List<Performance>{new Performance("hamlet", 55),
+            var invoice = new Invoice("BigCo", new List<Performance>{new Performance("hamlet", 55),
                 new Performance("as-like", 35),
                 new Performance("othello", 40)});
             
-            StatementPrinter statementPrinter = new StatementPrinter();
+            var statementPrinter = new StatementPrinter();
             
             // Not implemented yet
             // var result = statementPrinter.PrintAsHtml(invoice, plays);
@@ -50,16 +50,16 @@ namespace TheatricalPlayersRefactoringKata.Tests
 
         [Test]
         [UseReporter(typeof(DiffReporter))]
-        public void test_statement_with_new_play_types()
+        public void Test_statement_with_new_play_types()
         {
             var plays = new Dictionary<string, Play>();
             plays.Add("henry-v", new Play("Henry V", "history"));
             plays.Add("as-like", new Play("As You Like It", "pastoral"));
 
-            Invoice invoice = new Invoice("BigCoII", new List<Performance>{new Performance("henry-v", 53),
+            var invoice = new Invoice("BigCoII", new List<Performance>{new Performance("henry-v", 53),
                 new Performance("as-like", 55)});
             
-            StatementPrinter statementPrinter = new StatementPrinter();
+            var statementPrinter = new StatementPrinter();
 
             Assert.Throws<Exception>(() => statementPrinter.Print(invoice, plays));
         }
